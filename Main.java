74a32aba3a9c5d9f0d86dd588d68bef67a1cd52c
EvdISolo <Sagalakov1998ss@mail.ru>
2025-05-31 21:10:25 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,66 +5,78 @@ import java.util.*;
 
 
         public class Main {
-                    public static void main(String[] args) {
+            public static void main(String[] args) {
 
-
-                        MyMap<String, Integer> costPerAddress = new MyMap<>();
-                        costPerAddress.put("Абакан",400);
-                        costPerAddress.put("Москва",200);
-                        costPerAddress.put("Красноярск",300);
-
-
-                Scanner scanner = new Scanner(System.in);
-
-                while (true) {
-
-                    System.out.println(" Главное меню для заполнения  нового заказа  " + "\n" +
-                            " 0.Выход из программы\n" +
-                            " 1.Указать страну:\n " +
-                            "2.Указать город: \n " +
-                            "3.Указать вес (кг)");
+                HashMap<String, Integer> costPerAddress = new HashMap<>();
+                costPerAddress.put("Абакан", 400);
+                costPerAddress.put("Москва", 200);
+                costPerAddress.put("Красноярск", 300);
 
 
 
-                    String input = scanner.nextLine();
-                    int operation = Integer.parseInt(input);
+                    Scanner scanner = new Scanner(System.in);
+
+                    int weight = 0;
+                    while (true) {
+
+                        System.out.println(" Главное меню для заполнения  нового заказа  " + "\n" +
+                                " 0.Выход из программы\n" +
+                                " 1.Указать страну:\n " +
+                                "2.Указать город: \n " +
+                                "3.Указать вес (кг)");
 
 
-                    switch (operation) {
-                        case 0:
-                            System.out.println("Программа закрыта");
-                            System.exit(0);
-
-                        case 1:
-                            System.out.println("Введите название страны");
-                            String country = scanner.nextLine();
+                        String input = scanner.nextLine();
+                        int operation = Integer.parseInt(input);
 
 
+                        switch (operation) {
+                            case 0:
+                                System.out.println("Программа закрыта");
+                                System.exit(0);
+
+                            case 1:
+                                System.out.println("Введите название страны");
+                                String country = scanner.nextLine();
 
 
-                            break;
+                                break;
 
-                        case 2:
-                            System.out.println("Введите название города :");
-                            String city=scanner.nextLine();
+                            case 2:
+                                System.out.println("Введите название города :");
+                                String city = scanner.nextLine();
 
-                            break;
 
-                        case 3:
-                            System.out.println("Введите вес:");
-                            int weight= Integer.parseInt(scanner.nextLine());
 
-                            System.out.println("Стоимость доставки составит"+ );
-                            break;
+
+                                break;
+
+                            case 3:
+                                System.out.println("Введите вес:");
+                                weight = Integer.parseInt(scanner.nextLine());
+                                for (String key : costPerAddress.keySet()) {
+                                    int value = costPerAddress.get(key);
+                                System.out.println("Стоимость доставки составит " + value * weight);}
+                                break;
+                        }
+
+
 
                     }
+
+
 
 
                 }
 
 
-            }
-        }
+                }
+
+
+
+
+
+
 
 
 
